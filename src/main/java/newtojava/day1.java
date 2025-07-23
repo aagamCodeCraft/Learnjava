73c589c1f0fee174d9ddff638fe6d1ec40b11f98
aagam <aagam@192.168.1.7>
2025-07-23 19:30:59 +0530

    new change

--- src/main/java/newtojava/day1.java
+++ src/main/java/newtojava/day1.java
@@ -3,7 +3,7 @@ package newtojava;
 public class day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello aagam! Lets start learning");
 
 	}
 
